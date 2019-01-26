52dac092c7c5aca6ad8e7e172c82640605ce700d
blonskjulia <g.julia82@gmail.com>
2019-01-26 16:12:39 +0200

    add logger ang logFile

--- addressbook_test/src/test/java/addressbook_tR/test/TestBase.java
+++ addressbook_test/src/test/java/addressbook_tR/test/TestBase.java
@@ -2,20 +2,39 @@ package addressbook_tR.test;
 
 import addressbook_tR.manage.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+
 
 public class TestBase {
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     @BeforeSuite
     public void setUp() {
         app.start();
     }
 
+    @BeforeMethod
+    public void testLogStart(Method m) {
+        logger.info("Start test: " + m.getName());
+    }
+
     @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
+    }
+
+    @AfterMethod
+    public void testLogStop(Method m) {
+        logger.info("Stop test: " + m.getName());
     }
 }
 
